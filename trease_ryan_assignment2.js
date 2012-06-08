d549c07c023ce18e24dd87a257392d0f91e654fb
Ryan Trease <rstrease@fullsail.edu>
2012-06-07 23:10:31 -0500

    added functions for getDestination & getFishingWeather

--- trease_ryan_assignment2.js
+++ trease_ryan_assignment2.js
@@ -1,22 +1,29 @@
-//alert("JavaScript works!");
-
 var fishTypes = ["crappie", "bass"];
 var fishLimits = [5, 6];
-var location = ["our farm pond", "the lake"];
 var time = "6:00 AM";
+var rod = "Spinning Reel";
+var lure = "Rooster Tail";
+var bite = true;
+var weight = 2;
 
 var output = function (message) {
 	 console.log(message);
 };
 
-output("The location where I chose to go fishing is at " + location[1] + ".");
+var getDestination = function (locale) {
+	var destination = "" + locale ;
+	return destination;
+}
+var fishingHole = getDestination("the lake");
+(fishingHole);
+
 output("I wake up bright and early at " + time + " in order to get up before the fish start biting.");
+output("I have decided to fish at " + fishingHole + ".")
 output("Oops, Better check the weather first!");
 
 var getFishingWeather = function (unit) {
-	var temp = 98;
+	var temp = 75;
 	var weather;
-	var fishingWeather;
 		if (temp >= 65 && temp <= 85) {
 		weather = "clear";
 		}
@@ -26,19 +33,18 @@ var getFishingWeather = function (unit) {
 		else {
 			weather = "too hot";
 		}
-fishingWeather = "The weather is " + weather + " with a temperature of " + temp + "\u00B0 " + unit + ".";
+var fishingWeather = "The weather is " + weather + " with a temperature of " + temp + "\u00B0 " + unit + ".";
 return fishingWeather;
 }
 var weatherReport = getFishingWeather("fahrenheit");
 output(weatherReport);
 
-if (getFishingWeather){
-	output("Let's go fishing!");
-}
-else {
-	output("Let's go back to bed!");
-}
+if (weather = "clear") {
+	output("Time to gather the gear and head out!");
+	output("I grab the following equipment: " + rod + " and " + lure + " and head to " + fishingHole + ".");
+	output("Once at " + fishingHole + "," + " I cast out my line and wait for a fish to bite!");
 
+	
 var catchAFish = function () {
 	var fishLimit = fishLimits[fishNumber];
 	var fishType = fishTypes[fishNumber];
@@ -49,6 +55,11 @@ var catchAFish = function () {
 }
 output("I've caught my limit of " + fishType + ".")
 };
+
 for (var fishNumber = 0; fishNumber < fishTypes.length; fishNumber++) {
 catchAFish();
+}
+}
+else {
+	output("Time to go back to bec and try again tomorrow.");
 }
