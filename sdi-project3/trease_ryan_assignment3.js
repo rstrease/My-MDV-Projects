ad36000e2771ce19acb98a5db44c869385fa9cbe
Ryan Trease <rstrease@fullsail.edu>
2012-06-13 23:23:03 -0500

    Added buyMinnows function to the javascript file
    code determines if the bait shop is open and if they have any mono's
    left for sale.

--- sdi-project3/trease_ryan_assignment3.js
+++ sdi-project3/trease_ryan_assignment3.js
@@ -1,5 +1,26 @@
 //Ryan Trease
 //June 12th 2012
 //SDI Project 3
+//Taking the kids fishing
 
 //alert("JavaScript works!");
+
+var haveMinnows = false;
+var storeOpen = true;
+var message;
+var minnows = 36;
+
+var buyMinnows = function (message) {
+	if (haveMinnows && storeOpen) {var message = "Thankfully the bait shop is open and they have minnows for the fishing trip. We buy " + minnows + " minnows for the kids to use and head to the .",
+	theyHaveMinnows = message;
+	return theyHaveMinnows;
+	}
+	else if (haveMinnows === false && storeOpen === true) {
+		console.log("The bait shop is open but they are unfortunately out of minnows. The fishing trip is over before it starts.");
+	}
+	else {
+		console.log("The bait shop is closed. The fishing trip is over before it starts.");
+	}
+}
+var GoFishing = buyMinnows(message);
+console.log(GoFishing);
