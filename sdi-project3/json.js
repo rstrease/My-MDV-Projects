76ad6c7e294f37e19fba8ebbd1cea961b6dbc185
Ryan Trease <rstrease@fullsail.edu>
2012-06-14 15:03:47 -0500

    added a json.js file to the repository
    it lists others at the pond and their gear

---
+++ sdi-project3/json.js
@@ -0,0 +1,19 @@
+var json = {
+	"others": [
+		{
+			"name": "Brett",
+			"pole": "Zebco 33",
+			"tackle": "rooster tail"
+		},
+		{
+			"name": "Taylor",
+			"pole": "SpongeBob Squarepants",
+			"tackle": ["bobber", " lead weight", " and hook"]
+		},
+		{
+			"name": "Shane",
+			"pole": "Spinning Reel",
+			"tackle": "spinner bait"
+		}
+	]
+};
