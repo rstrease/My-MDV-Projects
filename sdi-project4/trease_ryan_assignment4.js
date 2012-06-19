f456cc30b546b850bc79ff07d43a71f40b19c239
Ryan Trease <rstrease@fullsail.edu>
2012-06-19 12:53:38 -0500

    Added totalValue function to the javascript file
    Find the total value of just the numbers in an array, even if some of
    the items are not numbers

--- sdi-project4/trease_ryan_assignment4.js
+++ sdi-project4/trease_ryan_assignment4.js
@@ -7,13 +7,30 @@ var smallestValue = function (array, num) {
 	for (i = 0; i < array.length; i++) {
 		if (array[i] < smallest && array[i] > num) {
 			smallest = array[i];
-			return smallest;
 		}
 	}
-	
+	return smallest;
 };
 
 var newArray = [10, 36, 48, -5, 7, 40];
 var num = 39;
 
 console.log(smallestValue(newArray, num));
+
+
+//Find the total value of just the numbers in an array, even if some of the items are not numbers.
+
+var totalValue = function (array) {
+	var sum = 0;
+	for (i = 0; i < array.length; i++) {
+		if (!isNaN(array[i])) {
+			sum += array[i];			
+		};
+	};
+	return sum;
+};
+
+var a = new Array();
+a = [5,"a", 8, 10, "hello", 20];
+
+console.log(totalValue(a));
