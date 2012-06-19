46f40381210719330591412bba4e3904284272d0
Ryan Trease <rstrease@fullsail.edu>
2012-06-18 21:04:00 -0500

    added smallestValue function for the library
    Find the smallest value in an array that is greater than a given number

--- sdi-project4/trease_ryan_assignment4.js
+++ sdi-project4/trease_ryan_assignment4.js
@@ -1,1 +1,19 @@
 //alert("JavaScript works!");
+
+//Find the smallest value in an array that is greater than a given number
+
+var smallestValue = function (array, num) {
+	var smallest = array[2];
+	for (i = 0; i < array.length; i++) {
+		if (array[i] < smallest && array[i] > num) {
+			smallest = array[i];
+			return smallest;
+		}
+	}
+	
+};
+
+var newArray = [10, 36, 48, -5, 7, 40];
+var num = 39;
+
+console.log(smallestValue(newArray, num));
