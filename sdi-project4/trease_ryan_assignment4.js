6cdd0f87f1186796b52d063992f41b72ec38bc3a
Ryan Trease <rstrease@fullsail.edu>
2012-06-21 13:09:31 -0500

    Added function numbers to the javascript file

--- sdi-project4/trease_ryan_assignment4.js
+++ sdi-project4/trease_ryan_assignment4.js
@@ -1,5 +1,6 @@
 //alert("JavaScript works!");
 
+//function 1
 //Does a string follow a 123-456-7890 pattern like a phone number?
 
 var validPhone = function (string){
@@ -15,7 +16,7 @@ var validPhone = function (string){
 var num = "405582-5423";
 console.log(validPhone(num));
 
-
+//function 2
 //Does a string follow an aaa@bbb.ccc pattern like an email address?
 
 var validEmail = function (string) {
@@ -31,7 +32,7 @@ var validEmail = function (string) {
 var mail = "rstrease@gmail.com";
 console.log(validEmail(mail));
 
-
+//function 3
 //Is the string a URL? (Does it start with http: or https:?)
 
 	var urlString = function (url) {
@@ -46,7 +47,7 @@ console.log(validEmail(mail));
 	
 console.log(urlString("http://ww.google.com"))
 
-
+//function 4
 //Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
 
 var formatNum = function (number) {
@@ -57,7 +58,7 @@ var formatNum = function (number) {
 var num = 5.7;
 console.log("The number " + num + " formatted to two decimal places is $" + formatNum(num) + ".");
 
-
+//function 5
 //Given a string version of a number such as "42", return the value as an actual Number, such as 42.
 
 	var stringVersion = function (num){
@@ -68,7 +69,7 @@ console.log("The number " + num + " formatted to two decimal places is $" + form
 var number = "42";
 console.log("The string \"" + number + "\" returned as an actual number is " + stringVersion(number) + ".");
 
-
+//function 6
 //Find the smallest value in an array that is greater than a given number
 
 var smallestValue = function (array, num) {
@@ -86,7 +87,7 @@ var num = 39;
 
 console.log("The smallest value in the array that is greater than " + num + " is " + smallestValue(newArray, num) + ".");
 
-
+//function 7
 //Find the total value of just the numbers in an array, even if some of the items are not numbers.
 
 var totalValue = function (array) {
